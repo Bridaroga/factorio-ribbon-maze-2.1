2147d3ab2e0dbb5c90d5a5b1caa6a603247241ac
Raphal <931674+Bridaroga@users.noreply.github.com>
2026-07-26 22:15:33 -0500

    Fixed [maze-terraforming-target] icon.

--- prototypes/entity/maze-terraforming-target.lua
+++ prototypes/entity/maze-terraforming-target.lua
@@ -24,14 +24,19 @@ data:extend({
     {
         type = "simple-entity-with-force",
         name = "maze-terraforming-target",
-        icon = "__base__/graphics/icons/wall.png",
+        icons = {
+            {
+                icon = "__base__/graphics/icons/wall.png",
+                icon_size = 64,
+                tint = { b = 1 },
+            }
+        },
         picture = {
             filename = "__base__/graphics/entity/pipe/pipe-cross.png",
             priority = "extra-high",
             width = 1,
             height = 1,
         },
-        icon_size = 32,
         collision_mask = { layers = { is_object = true } },
         collision_box = { {-0, -0}, {0, 0} },
         selection_box = { {-1, -1}, {2.9, 2.9} },
